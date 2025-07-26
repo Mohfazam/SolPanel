f2103884ac8bbdf0e9c871fbdef89bf6f45d4b46
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-27 02:27:46 +0530

    Button

--- Frontend/src/Components/Card/RequestAirDrop.tsx
+++ Frontend/src/Components/Card/RequestAirDrop.tsx
@@ -1,4 +1,4 @@
-import { Download  } from "lucide-react";
+import { Download } from "lucide-react";
 import { CardTop } from "../UI/CardTop";
 import { useWallet, useConnection } from "@solana/wallet-adapter-react";
 
@@ -9,6 +9,7 @@ import { NumberInput } from "../UI/NumberInput";
 import { Button } from "../UI/Button";
 
 export const RequestAirdrop = () => {
+    const [isLoading, setIsLoading] = useState(false);
     const [solBal, setSolBal] = useState(0);
     const [refreshBalance, setRefreshBalance] = useState(false);
 
@@ -17,47 +18,56 @@ export const RequestAirdrop = () => {
     const [solPrice, setSolPrice] = useState(0);
     const { connected } = useWallet();
     const wallet = useWallet();
-    const {connection} = useConnection();
+    const { connection } = useConnection();
     const publicKey = wallet.publicKey;
 
     useEffect(() => {
         const fetchBalance = async () => {
-            if(publicKey){
-                try{
+            if (publicKey) {
+                try {
                     const lamports = await connection.getBalance(publicKey);
-                    setSolBal(lamports/LAMPORTS_PER_SOL);
-                } catch(error){
+                    setSolBal(lamports / LAMPORTS_PER_SOL);
+                } catch (error) {
                     console.error("Error fetching Balance: ", error);
                 }
             }
         };
 
         const fetchSolPrice = async () => {
-            try{
+            try {
                 const response = await axios.get("https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=usd");
                 console.log(response.data.solana.usd);
                 setSolPrice(response.data.solana.usd);
-            } catch(error){
+            } catch (error) {
                 console.log("Error fetching price: ", error);
             }
 
         }
-        
+
         fetchSolPrice();
         fetchBalance();
     }, [connected, solBal, publicKey, refreshBalance]);
 
     const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setInput(e.target.value);
+
+        const value = parseFloat(e.target.value);
+        if (value > 5) {
+            setInput("5");
+        } else {
+
+            setInput(e.target.value);
+        }
     }
-    
+
 
     const handleAirDrop = async () => {
         const amount = parseFloat(input);
 
-        if(!publicKey) return
+        if (!publicKey) return;
 
-        try{
+        setIsLoading(true);
+
+        try {
             const signature = await connection.requestAirdrop(
                 publicKey,
                 amount * LAMPORTS_PER_SOL
@@ -66,8 +76,11 @@ export const RequestAirdrop = () => {
             await connection.confirmTransaction(signature, "confirmed");
             alert("Air Drop Successfull");
             setRefreshBalance(!refreshBalance);
-        } catch(error){
+        } catch (error) {
             console.log("Airdrop failed error: " + error);
+        }
+        finally {
+            setIsLoading(false);
         }
     }
 
@@ -75,14 +88,23 @@ export const RequestAirdrop = () => {
     return (
         <div className="bg-[#141414] border border-[#2a2a2a] rounded-xl p-6 h-[381px]">
             <div className="mb-12">
-                <CardTop Text="Request Airdrop" Icon={<Download  size={24} />} />
+                <CardTop Text="Request Airdrop" Icon={<Download size={24} />} />
             </div>
 
-            <NumberInput Text="Amount (SOL)" Max={5} Placeholder="Enter amount (max 5)" onChange={handleInput}/>
+            {connected ? (<div>
+                <NumberInput Text="Amount (SOL)" Max={5} Placeholder="Enter amount (max 5)" onChange={handleInput} />
 
-            <div className="flex justify-center items-center w-full mt-3">
-                <Button onClick={handleAirDrop} text="Request AirDrop" disable={input.length === 0 ? true : false}/>
+                <div className="flex justify-center items-center w-full mt-3">
+                    <Button onClick={handleAirDrop} text="Request AirDrop" disable={isLoading} />
+                </div>
             </div>
+            ) : (
+                <div className="flex justify-center items-center text-white w-full  px-4 py-6 bg-[#141414] border border-[#2a2a2a] rounded-xl">
+                    Connect the Wallet First
+                </div>
+            )}
+
+
         </div>
     );
 };
